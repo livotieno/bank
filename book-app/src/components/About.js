acfdeae9d91330f791b512da6b81c9e3bbe0db45
livotieno <livio5261@gmail.com>
2023-11-17 19:19:40 +0300

    added a paragraph

--- book-app/src/components/About.js
+++ book-app/src/components/About.js
@@ -8,9 +8,9 @@ const About = () => {
       <p>3LIV BOOKS is another well-known book summary app that offers summaries in text, audio, and animated formats. It has a comprehensive library of summaries of nonfiction bestseller books, and its summaries provide key insights of the books in minutes. This site provides brief description concerning the company</p>
       <p>The job vacanicies are as follows</p>
       <ol>
-        <li>1: Cleaners - Salary per month 15,000</li>
-        <li>2: Book Loaders - Salary per month 20,000</li>
-        <li>3: Drivers - Salary per month 30,000</li>
+        <li> Cleaners - Salary per month 15,000</li>
+        <li> Book Loaders - Salary per month 20,000</li>
+        <li> Drivers - Salary per month 30,000</li>
       </ol>
       <p>To apply for a job you need to have a valid ID card and passport</p>
       <p>Location for the interview will be at the main company along Thika road from 8:00AM.</p>
--- book-app/src/components/Services.js
+++ book-app/src/components/Services.js
@@ -2,9 +2,10 @@ import React from 'react';
 
 const Services = () => {
  return (
-    <div>
+    <div className='services'>
       <h1>Our Services</h1>
       <p>We offer quality services worlwide hence the workers get a health insuarance.  </p>
+      <p>Worlwide our services run from 9:00AM to 3:00PM so that our staff may have enough time to bond with their family.</p>
     </div>
  );
 };
